97305f2cbf9ddaa73f1d89cda6ca54c64da575a7
Cesar Goulart <devcesargoulart@gmail.com>
2024-11-18 21:31:47 -0300

    feat: style with css

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 import Primeiro from './basicos/Primeiro';
 import ComParametro from './basicos/ComParametros';
@@ -8,26 +9,28 @@ import Card from './layout/Card';
 
 // Utilizando Função arrow para retornar
 export default () => (
-  <div id="app">
+  <div className="App" id="App">
     <h1>Fundamentos React</h1>
-    <Card titulo="#4 - Desafio números aleatórios">
-      <Aleatorio />
-    </Card>
+    <div className="Cards">
+      <Card titulo="#4 - Desafio números aleatórios">
+        <Aleatorio />
+      </Card>
 
-    <Card titulo="#3 - Trabalhando com Fragmentos">
-      <Fragmento />
-    </Card>
+      <Card titulo="#3 - Trabalhando com Fragmentos">
+        <Fragmento />
+      </Card>
 
-    <Card titulo="#2 - Com parametro">
-      <ComParametro
-        titulo="Segundo Componente"
-        subtitulo="passando parametros para o filho"
-        numero={9.3}
-      />
-    </Card>
+      <Card titulo="#2 - Com parametro">
+        <ComParametro
+          titulo="Segundo Componente"
+          subtitulo="passando parametros para o filho"
+          numero={9.3}
+        />
+      </Card>
 
-    <Card titulo="#1 - Primeiro Componente">
-      <Primeiro />
-    </Card>
+      <Card titulo="#1 - Primeiro Componente">
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
