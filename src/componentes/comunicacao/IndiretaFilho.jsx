5386c5bce96418d94e79419b423b72d9fe5eb242
Cesar Goulart <devcesargoulart@gmail.com>
2024-11-19 00:17:39 -0300

    feat: usando o hook de estado (useState) para renderizar tela com o conteudo passado do filho para o pai

--- src/componentes/comunicacao/IndiretaFilho.jsx
+++ src/componentes/comunicacao/IndiretaFilho.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-
+let teste = 0;
 // componente do tipo funcional
 export default (props) => {
+  const min = 50;
+  const max = 70;
+  const gerarIdade = () => parseInt(Math.random() * (max - min)) + min;
+  const gerarNerd = () => Math.random() > 0.5;
+  teste++;
   return (
     <div>
       <div> Filho</div>
-      <button onClick={() => props.quandoClicar('João', 53, true)}>
+      <button
+        onClick={() =>
+          props.quandoClicar('João_' + teste, gerarIdade(), gerarNerd())
+        }
+      >
         Fornecer Informações
       </button>
     </div>
--- src/componentes/comunicacao/IndiretaPai.jsx
+++ src/componentes/comunicacao/IndiretaPai.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IndiretaFilho from './IndiretaFilho';
 
 // componente do tipo funcional
 export default () => {
+  const [nome, setNome] = useState('?');
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
   const fornecerInformacoes = (nome, idade, nerd) => {
-    console.log(nome, idade, nerd);
+    setNome(nome);
+    setIdade(idade);
+    setNerd(nerd);
   };
   return (
     <div>
-      <p>Comunicação do filho para o pai via função callback</p>
+      <p>Comunicação do filho para o pai via função callback e Estado</p>
+      <div>
+        <span>{nome} </span>
+        <span>
+          <strong>{idade} </strong>
+        </span>
+        <span>{nerd ? 'Verdadeiro' : 'Falso'}</span>
+      </div>
       <IndiretaFilho quandoClicar={fornecerInformacoes} />
     </div>
   );
