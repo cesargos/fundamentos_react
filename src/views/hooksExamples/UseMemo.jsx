b4e9bd0f22af83f2d86852381ade1c1d562c32eb
Cesar Goulart <devcesargoulart@gmail.com>
2024-12-26 13:23:21 -0300

    useMemo

--- src/views/hooksExamples/UseMemo.jsx
+++ src/views/hooksExamples/UseMemo.jsx
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
+
+function sum(a, b) {
+  const future = Date.now() + 2000;
+  // eslint-disable-next-line no-empty
+  while (Date.now() < future) {} // espera de 2s (processamento pesado simulação)
+  return Number(a) + Number(b);
+}
 
 const UseMemo = () => {
+  const [n1, setN1] = useState(0);
+  const [n2, setN2] = useState(0);
+  const [n3, setN3] = useState(0);
+
+  //1 - ¹const result = sum(n1, n2);
+
+  // 2 - const [result, setResult] = useState(0);
+  // useEffect(() => {
+  //   setResult(sum(n1, n2));
+  // }, [n1, n2]);
+
+  //3
+  const result = useMemo(() => sum(n1, n2), [n1, n2]);
+
   return (
     <div className="UseMemo">
       <PageTitle title="Hook UseMemo" subtitle="Retorna um valor memoizado!" />
+      <div className="center">
+        <input
+          type="number"
+          className="input"
+          value={n1}
+          onChange={(e) => setN1(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={n2}
+          onChange={(e) => setN2(parseInt(e.target.value))}
+        />
+        <input
+          type="number"
+          className="input"
+          value={n3}
+          onChange={(e) => setN3(parseInt(e.target.value))}
+        />
+        <span className="text">{result}</span>
+      </div>
+      <SectionTitle title="Anotações" />
+      <ul>
+        <li>
+          Quando usamos apenas o useState. Ao chamar novamente a função de
+          rendereização, caso ela rode uma função demorada, ele vai travar a
+          tela ate ser finalizada. E o input N3 vai ficar congelado e travando
+          com a chamada da função demorada indevidamente mesmo ele não fazendo
+          parte da função.¹
+          <ol>
+            <li>
+              Podemos utilizar o useEffect e colocar a função que demora nele².
+              Mas assim só vai deixar de travar quando usar o n3 caso use os
+              outros 2 ele vai continuar travando
+            </li>
+            <li>
+              Podemos usar o useMemo para resolver de forma mais profissional³.
+              Assim não teremos que usar o useEffect junto com o useState. Mas
+              continua travando
+            </li>
+          </ol>
+        </li>
+        <li>
+          É um falor em cache que só vai chamar a função novamente. Caso os
+          valores que estão no array se modifique
+        </li>
+      </ul>
     </div>
   );
 };
