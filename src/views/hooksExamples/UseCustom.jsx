e63b715d4069e170335f23bb6f2d586e54ec2a0d
Cesar Goulart <devcesargoulart@gmail.com>
2024-12-31 17:59:16 -0300

    use custom hook example 2 - useFetch

--- src/views/hooksExamples/UseCustom.jsx
+++ src/views/hooksExamples/UseCustom.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import PageTitle from '../../components/layout/PageTitle';
 import SectionTitle from '../../components/layout/SectionTitle';
 import { useCounter } from '../../hooks/useCounter';
+import { useFetch } from '../../hooks/useFetch';
 
 const UseRef = () => {
   const [count, inc, dec] = useCounter();
+  const url = 'http://files.cod3r.com.br/curso-react/estados.json';
+  const response = useFetch(url);
+
+  function showStates(states) {
+    return states.map((state) => (
+      <li key={state.nome + state.sigla}>
+        {state.nome} - {state.sigla}
+      </li>
+    ));
+  }
+
   return (
     <div className="UseCustom">
       <PageTitle
@@ -23,6 +35,15 @@ const UseRef = () => {
             +1
           </button>
         </div>
+      </div>
+
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        {!response.loading ? (
+          <ul>{showStates(response.data)}</ul>
+        ) : (
+          <h1>Carregando...</h1>
+        )}
       </div>
 
       <SectionTitle title="Anotações" />
